9ead28d4c712a5339fdf8d42c4d87a2a6a092b78
Eliza <luiza.trdatyan@student.rau.am>
2025-09-13 15:52:51 +0400

    second homework fixed version

--- ENV-1.2/task1.cpp
+++ ENV-1.2/task1.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
-#include <cstdlib> // для atoi
+#include <stdexcept>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    int c = atoi(argv[3]);
+    if (argc != 4) {
+        cout << "Usage: " << argv[0] << " num1 num2 num3" << endl;
+        return 1;
+    }
+
+    int a, b, c;
+
+    try {
+        a = stoi(argv[1]);
+        b = stoi(argv[2]);
+        c = stoi(argv[3]);
+    } catch (const invalid_argument& e) {
+        cout << "Error: One of the inputs is not a valid integer." << endl;
+        return 1;
+    } catch (const out_of_range& e) {
+        cout << "Error: One of the inputs is out of the valid integer range." << endl;
+        return 1;
+    }
 
     int max = a, min = a;
 
--- ENV-1.2/task2.cpp
+++ ENV-1.2/task2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    for (int i = argc - 1; i >= 1; --i) {
+    for (int i = argc - 1; i >= 0; --i) {
         std::cout << argv[i] << " ";
     }
     std::cout << std::endl;
