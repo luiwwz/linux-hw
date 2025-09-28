d4d63394a4a618477c93dddad9274ac073897f2f
Eliza <luiza.trdatyan@student.rau.am>
2025-09-29 02:44:47 +0400

    simple-copy.cpp

--- FS-2/simple-copy.cpp
+++ FS-2/simple-copy.cpp
@@ -1,24 +1,25 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
     if (argc != 3) {
         const char* msg = "Usage: ./simple-copy <source> <destination>\n";
-        write(2, msg, 44); 
+        write(STDERR_FILENO, msg, strlen(msg));
         return 1;
     }
 
     int fd_src = open(argv[1], O_RDONLY);
     if (fd_src < 0) {
         const char* msg = "Error: cannot open source file\n";
-        write(2, msg, 33);
+        write(STDERR_FILENO, msg, strlen(msg));
         return 1;
     }
 
     int fd_dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd_dst < 0) {
         const char* msg = "Error: cannot open/create destination file\n";
-        write(2, msg, 44);
+        write(STDERR_FILENO, msg, strlen(msg));
         close(fd_src);
         return 1;
     }
