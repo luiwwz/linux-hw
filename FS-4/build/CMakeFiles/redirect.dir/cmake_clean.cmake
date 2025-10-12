f5f7350cc3812229ea31479d3bec1993741df6a2
Eliza <luiza.trdatyan@student.rau.am>
2025-10-12 23:15:41 +0400

    adding FS-4

---
+++ FS-4/build/CMakeFiles/redirect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/redirect.dir/redirect.cpp.o"
+  "CMakeFiles/redirect.dir/redirect.cpp.o.d"
+  "redirect"
+  "redirect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/redirect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
