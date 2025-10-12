bc1f820bef2b637604d7a140c25d7d9f0e0a7b3a
Eliza <luiza.trdatyan@student.rau.am>
2025-10-12 23:01:43 +0400

    adding FS-3

---
+++ FS-3/build/CMakeFiles/hacker-rm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hacker-rm.dir/hacker-rm.cpp.o"
+  "CMakeFiles/hacker-rm.dir/hacker-rm.cpp.o.d"
+  "hacker-rm"
+  "hacker-rm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hacker-rm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FS-3/build/CMakeFiles/hacker-rm.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for hacker-rm.
+# This may be replaced when dependencies are built.
